c222530bcf0c60a73b6776870c3d956e1782c1f0
sude nur <sudenur@sabanciuniv.edu>
2023-07-17 16:12:11 +0300

    Initial Preparation of the train.csv file.

---
+++ Main_code.py
@@ -0,0 +1,50 @@
+# ----------------------------------------- Install -----------------------------------------
+import pandas as pd
+import numpy as np
+from sklearn.impute import KNNImputer
+
+# ----------------------------------------- Check Documents -----------------------------------------
+
+train_csv = "train.csv"
+train = pd.read_csv(train_csv)
+
+print("Table: \n", train.head(10))
+print("Table properties: \n", train.describe())
+print("Table shape (rows, columns): \n", train.shape)
+## 58 columns, 56 cathegories with EJ being categorical and rest as integer
+## 618 rows, 617 training
+print("Empty values: \n", train.isnull().sum())
+
+
+## BQ : 60, CB : 2, CC : 3, DU : 1, EL : 60, FC : 1, FL : 1, FS : 2, GL : 1  --> EMPTY VALUES
+
+# ----------------------------------------- Fill empty values with KNN -----------------------------------------
+
+df_train = train.copy(deep=True)
+
+# Separate the sample names column (first column) from the rest of the data
+df_train_strings = df_train.iloc[:, :1]
+df_train_ints = df_train.iloc[:, 1:]
+
+# Separate categorical and numerical data
+categorical_data = df_train_ints.select_dtypes(include="object")
+numeric_data = df_train_ints.select_dtypes(include="number")
+
+# Initialize the KNN imputer
+imputer = KNNImputer(weights="distance", add_indicator=True)
+
+# Fit and transform the imputer on the numeric data
+imputed_numeric_data = imputer.fit_transform(numeric_data)
+
+# Get the feature names including the added indicator columns
+imputed_columns = imputer.get_feature_names_out(input_features=numeric_data.columns)
+
+# Convert the imputed numeric data back to a DataFrame
+imputed_df = pd.DataFrame(imputed_numeric_data, columns=imputed_columns)
+
+# Concatenate the imputed numeric data with the categorical data
+imputed_df = pd.concat([df_train_strings, imputed_df, categorical_data], axis=1)
+
+print("Empty values: \n", imputed_df.isnull().sum())
+
+# ----------------------------------------- Visualizations -----------------------------------------
