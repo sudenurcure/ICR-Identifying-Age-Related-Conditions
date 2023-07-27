47130341670827ed811293aac9d3af90705a0f96
sude nur <sudenur@sabanciuniv.edu>
2023-07-28 00:14:16 +0300

    PCA Analysis

--- Main_code.py
+++ Main_code.py
@@ -27,30 +27,37 @@ print("Empty values: \n", train.isnull().sum())
 # ----------------------------------------- Fill empty values with KNN -----------------------------------------
 
 df_train = train.copy(deep=True)
+df_train["Class"] = df_train["Class"].replace(
+    {0: "Healthy", 1: "Sick"}
+)  # Turn Class to object dtype
 
-# Separate the sample names column (first column) from the rest of the data
-df_train_strings = df_train.iloc[:, :1]
-df_train_ints = df_train.iloc[:, 1:]
-
-# Separate categorical and numerical data
-categorical_data = df_train_ints.select_dtypes(include="object")
-numeric_data = df_train_ints.select_dtypes(include="number")
+categorical_data = df_train.select_dtypes(exclude="number")
+numeric_data = df_train.select_dtypes(include="number")
 
 # Initialize the KNN imputer
-imputer = KNNImputer(weights="distance", add_indicator=True)
+imputer = KNNImputer(weights="distance", add_indicator=False)
 
 # Fit and transform the imputer on the numeric data
 imputed_numeric_data = imputer.fit_transform(numeric_data)
 
 # Get the feature names including the added indicator columns
-imputed_columns = imputer.get_feature_names_out(input_features=numeric_data.columns)
+imputed_columns = imputer.get_feature_names_out(numeric_data.columns)
 
 # Convert the imputed numeric data back to a DataFrame
 imputed_df = pd.DataFrame(imputed_numeric_data, columns=imputed_columns)
 
 # Concatenate the imputed numeric data with the categorical data
-imputed_df = pd.concat([df_train_strings, imputed_df, categorical_data], axis=1)
+df_train = pd.concat([categorical_data, imputed_df], axis=1)
+print("Table: \n", df_train.head(10))
+print(df_train.dtypes)
 
+
+def Save_datafile(df):
+    path = "/home/sudenurcure/ICR-Identifying-Age-Related-Conditions/imputed_df.csv"
+    df.to_csv(path)
+
+
+"""
 # ----------------------------------------- Label Encode Categorical Data -----------------------------------------
 LE = LabelEncoder()
 imputed_df["EJ"] = LE.fit_transform(imputed_df["EJ"])
@@ -59,79 +66,82 @@ print("Empty values: \n", imputed_df.isnull().sum())
 print("Table: \n", imputed_df.head(10))
 
 
+
 # ----------------------------------------- Corrolation and Visualizations -----------------------------------------
 
-vis_df = imputed_df.drop(imputed_df.filter(regex=r"missing|Id").columns, axis=1)
+"""
 
 
 def Correlation_map(df):
     print("Corrolation Heatmap: \n")
-
     corrolation = df.corr(method="spearman", min_periods=0)
     dataplot_hm = sns.heatmap(df.corr(method="spearman", min_periods=0))
     plt.show()
 
+
+def Box_plot(df):
     print("Box plot: ")
     df.boxplot(by="Class", layout=(8, 7), figsize=(15, 15))
 
 
-def Save_datafile(df):
-    path = "/home/sudenurcure/ICR-Identifying-Age-Related-Conditions/imputed_df.csv"
-    df.to_csv(path)
+Correlation_map(df_train)
 
 
-# Correlation_map(vis_df)
+def PCA_dim_reduction(x):
+    pca_df = PCA(n_components=2)
+    principalComponents_df = pca_df.fit_transform(x)
+    principal_table_Df = pd.DataFrame(
+        data=principalComponents_df,
+        columns=["principal component 1", "principal component 2"],
+    )
+    print(principal_table_Df.tail())
+    print(
+        "Explained variation per principal component: {}".format(
+            pca_df.explained_variance_ratio_
+        )
+    )
+    # 80% information is lost. PCA 1 caught only 12% and PCA 2 caught only 8% of information.
+
+    return principal_table_Df
 
 
-def PCA_dim_reduction(df):
-    X, sorted_eig_vectors = PCA_analysis(df)
-    eig_scores = np.dot(X, sorted_eig_vectors[:, :2])
-    return eig_scores, sorted_eig_vectors
+def PCA_plot(df, principal_table_Df):
+    plt.figure()
+    plt.figure(figsize=(10, 10))
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=14)
+    plt.xlabel("Principal Component - 1", fontsize=20)
+    plt.ylabel("Principal Component - 2", fontsize=20)
+    plt.title("Principal Component Analysis of ICR Dataset", fontsize=20)
+    targets = ["Healthy", "Sick"]
+    colors = ["r", "g"]
+    for target, color in zip(targets, colors):
+        indicesToKeep = df["Class"] == target
+        plt.scatter(
+            principal_table_Df.loc[indicesToKeep, "principal component 1"],
+            principal_table_Df.loc[indicesToKeep, "principal component 2"],
+            c=color,
+            s=50,
+        )
+
+    plt.legend(targets, prop={"size": 15})
 
 
 def PCA_analysis(df):
-    X = StandardScaler().fit_transform(df)
-    cov = (X.T @ X) / (X.shape[0] - 1)
-    eig_values, eig_vectors = np.linalg.eig(cov)
-    idx = np.argsort(eig_values)[::-1]
-    sorted_eig_vectors = eig_vectors[:, idx]
-    return X, sorted_eig_vectors
+    features = df.select_dtypes(include="number").columns
+    x = df.loc[:, features].values
+    x = StandardScaler().fit_transform(x)
+    print("shape: ", x.shape, "\nmean: ", np.mean(x), "\nstd: ", np.std(x))
+
+    feat_cols = ["feature" + str(i) for i in range(x.shape[1])]
+    normalised_df = pd.DataFrame(x, columns=feat_cols)
+    print(normalised_df.tail())
+    principal_table_Df = PCA_dim_reduction(x)
+    PCA_plot(df, principal_table_Df)
+    return 0
 
 
-def biplot(score, coeff, labels):
-    xs = score[:, 0]
-    ys = score[:, 1]
-    n = coeff.shape[0]
+PCA_analysis(df_train)
 
-    fig, ax = plt.subplots()
-
-    for i, u in enumerate(vis_df["Class"].unique()):
-        xi = score[vis_df["Class"] == u, 0]
-        yi = score[vis_df["Class"] == u, 1]
-        ax.scatter(xi, yi, label=u)
-
-    for i in range(n):
-        ax.arrow(
-            0, 0, coeff[i, 0], coeff[i, 1], color="r", head_width=0.05, head_length=0.1
-        )
-        ax.text(
-            coeff[i, 0] * 1.35,
-            coeff[i, 1] * 1.35,
-            labels[i],
-            color="g",
-            ha="center",
-            va="center",
-        )
-
-    plt.xlabel("PC{}".format(1))
-    plt.ylabel("PC{}".format(2))
-    plt.grid()
-    plt.legend()
-
-
-# Assuming vis_df is a global DataFrame variable containing the original data
-X, sorted_eig_vectors = PCA_dim_reduction(vis_df)
-biplot(X, sorted_eig_vectors, vis_df.columns)
-plt.show()
 
 # %%
